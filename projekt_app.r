9b3383a49a264109fccdab6f00d1edd8ebb81cab
antoni <antonirakowski@proton.me>
2024-12-17 23:04:57 +0100

    Co-authored-by: Jan Taran <Pablo1337PL@users.noreply.github.com>

--- projekt_app.r
+++ projekt_app.r
@@ -2,12 +2,49 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 
-ui <- fluidPage(
-
-    titlePanel("Nowy s*xy projekt")
-
+choice_labels <- list(
+  "Kalorie",
+  "Aktywność",
+  "Nauka",
+  "Sen",
+  "Kroki",
+  "Płyny",
+  "Zadowolenie"
 )
 
-server <- function(input, output) {}
+data <- read.csv("/home/antoni/Uni/Semestr-3/TWD/Projekty/Projekt-2/TWD-Project-2/data.csv")
+
+ui <- fluidPage(
+
+    titlePanel("very very s3xy project adasdasdasd"),
+
+    sidebarLayout(
+
+        sidebarPanel(
+
+            selectInput("y", "Wybierz wartości na osi OY", choices = choice_labels),
+
+            sliderInput("date", "Wybierz zakres czasu:", min = 1, max = 4, value = 3)
+        ),
+
+        mainPanel(
+
+            tabsetPanel(
+                id = "tabs",
+                tabPanel("Wykres punktowy", plotOutput("scatter"))
+            )
+        )
+    )
+)
+
+server <- function(input, output) {
+    
+  values <- reactive({
+
+    }
+  })
+
+}
+
 
 shinyApp(ui = ui, server = server)
