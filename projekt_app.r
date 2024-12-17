35c1693b50b744908f6e5f69056add09008e33b1
antoni <antonirakowski@proton.me>
2024-12-17 19:44:14 +0100

    empty app

---
+++ projekt_app.r
@@ -0,0 +1,13 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+ui <- fluidPage(
+
+    titlePanel("Nowy s*xy projekt")
+
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
