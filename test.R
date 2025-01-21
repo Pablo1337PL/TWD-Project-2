9e64d02c040c2373334df46a1e0551f077fa929c
Pablo1337PL <janp.taran@gmail.com>
2025-01-21 17:13:21 +0100

    deleting

--- test.R
+++
@@ -1,8 +0,0 @@
-setwd("/home/JanTar/Desktop/PW/Semestr3/TechnikiWizualizacjiDanych/TWD-Project-2/")
-getwd()
-db <- as.data.frame(read.csv("data.csv"))
-summary(db)
-import(dplyr)
-
-
-seq(1, 3, 0.25)
